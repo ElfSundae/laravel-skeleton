c934bcc5e5c34996a85ef8ff651db374ac36b3a9
Elf Sundae <elf.sundae@gmail.com>
2017-09-05 00:34:13 +0800

    Delete TestCase.php

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,8 +2,16 @@
 
 namespace :namespace\Test;
 
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+
 class ExampleTest extends TestCase
 {
+    protected function tearDown()
+    {
+        m::close();
+    }
+
     public function testExample()
     {
         $this->assertTrue(true);
